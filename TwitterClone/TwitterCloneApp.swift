0f50f3980adf171b03677c9341e00a2d0e95a796
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-28 15:00:42 +0100

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Khan on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
