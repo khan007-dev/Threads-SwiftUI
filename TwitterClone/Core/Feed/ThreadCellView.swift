4f8d9fde1d694e42820c4208219378e00735cfa8
abdulwariskhan <hellokhan.ch@gmail.com>
2024-03-01 19:56:49 +0100

    Completed Feed View

---
+++ TwitterClone/Core/Feed/ThreadCellView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadCellView.swift
+//  TwitterClone
+//
+//  Created by Khan on 01.03.2024.
+//
+
+import SwiftUI
+
+struct ThreadCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadCellView()
+}
