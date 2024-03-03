f56b6fdb9c4959ebfb64cadc8cebed1073562808
abdulwariskhan <hellokhan.ch@gmail.com>
2024-03-03 21:34:02 +0100

    Completed Edit Profile View

--- TwitterClone/Core/Profile/EditProfile/View/EditProfileView.swift
+++ TwitterClone/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -8,8 +8,81 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    
+                    HStack {
+                        VStack (alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            Text("AB Waris")
+                        }
+                       
+                        Spacer()
+                        CircularProfileImageView()
+                    }
+                    Divider()
+                    
+                    VStack (alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        TextField("Enter your Bio", text: $bio, axis: .vertical)
+                    }
+                    
+                    Divider()
+                    
+                    VStack (alignment: .leading) {
+                        Text("Link")
+                            .fontWeight(.semibold)
+                        TextField("Add link", text: $link, axis: .vertical)
+                    }
+                    
+                    Divider()
+                    
+                    Toggle("Private Profile", isOn: $isPrivateProfile)
+                    
+                    
+                }.font(.footnote)
+                    .padding()
+                    .background(.white)
+                    .cornerRadius(10)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(.systemGray4), lineWidth: 1)
+                    }
+                    .padding()
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }.font(.subheadline)
+                        .foregroundStyle(.black)
+                    
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        
+                    }.font(.subheadline)
+                        .foregroundStyle(.black)
+                    
+                }
+        }
+            
+        }
     }
 }
 
