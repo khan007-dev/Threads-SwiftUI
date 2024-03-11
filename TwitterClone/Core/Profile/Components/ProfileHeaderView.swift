32adf269337c4bfb73b1d7b7365e546e45e72d49
abdulwariskhan <hellokhan.ch@gmail.com>
2024-03-11 21:05:41 +0100

    Profile Navigation

---
+++ TwitterClone/Core/Profile/Components/ProfileHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHeaderView.swift
+//  TwitterClone
+//
+//  Created by Khan on 11.03.2024.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHeaderView()
+}
---
+++ TwitterClone/Core/Profile/CurrentUserProfile/View/CurrentUserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentUserProfileView.swift
+//  TwitterClone
+//
+//  Created by Khan on 11.03.2024.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
